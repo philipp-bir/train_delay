8217b1be7b623e95dd0f63b8a49d28324ea4fc65
philipp-bir <50335102+philipp-bir@users.noreply.github.com>
2019-08-25 19:08:55 -0500

    change endpoint URL, add a little ajax error handling

--- amtrak/urls.py
+++ amtrak/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('predict/train-id/<slug:train_id>/timestamp/<int:timestamp>', views.get_prediction,name="json_prediction"), 
-    path('predict/post',views.prediction_post,name='prediction_post'),
+    path('predict/',views.prediction_post,name='prediction_post'),
 ]
